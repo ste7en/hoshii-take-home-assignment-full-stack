5846ffdebef996b4767d858eee8891d083281ecc
Stefano Formicola <stefanoformicola@icloud.com>
2024-12-29 19:31:05 +0100

    feat: integrate EmailThread component into App layout
    - Added EmailThread component to the main App layout for displaying selected email threads.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Layout from './app/Layout'
 import { EmailProvider } from '@/features/email/context'
 import { UserProvider } from '@/features/user/context'
+import { EmailThread } from '@/features/email/components'
 
 function App() {
   return (
     <UserProvider>
       <EmailProvider>
         <Layout>
+          <EmailThread />
         </Layout>
       </EmailProvider>
     </UserProvider>
