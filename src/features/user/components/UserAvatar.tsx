bb01fed87de3127fb30a97222eddab7929079f28
Stefano Formicola <stefanoformicola@icloud.com>
2024-12-29 20:39:53 +0100

    refactor: enhance UserAvatar component with utility classnames
    - Updated UserAvatar component to utilize the `cn` utility for conditional classnames, improving styling flexibility.
    - Added rounded corners to the AvatarFallback for a more polished appearance.

--- src/features/user/components/UserAvatar.tsx
+++ src/features/user/components/UserAvatar.tsx
@@ -1,6 +1,7 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { getInitials, getColorFromString } from "../utils"
 import { User } from "../types"
+import { cn } from "@/lib/utils"
 
 interface UserAvatarProps {
   user: Pick<User, "name" | "email" | "avatar">
@@ -9,10 +10,10 @@ interface UserAvatarProps {
 
 export function UserAvatar({ user, className }: UserAvatarProps) {
   return (
-    <Avatar className={className}>
+    <Avatar className={cn("h-8 w-8 rounded-lg", className)}>
       <AvatarImage src={user.avatar} alt={user.name} />
       <AvatarFallback
-        className="text-white"
+        className="rounded-lg text-white"
         style={{ backgroundColor: getColorFromString(user.name) }}
       >
         {getInitials(user.name)}
