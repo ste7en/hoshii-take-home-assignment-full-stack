84dabf117e9276fdfd7d7c724f85ac630f8c777e
Stefano Formicola <stefanoformicola@icloud.com>
2024-12-30 10:42:22 +0100

    feat: add feature flags for environment variables
    - Introduced `ImportMetaEnv` interface to define environment variables for feature flags.
    - Created `FeatureFlags` module to evaluate and expose feature flags based on environment variable values, enabling conditional feature activation.

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_ENABLE_OTHER_INBOXES: string
+  readonly VITE_ENABLE_SEARCH: string
+  readonly VITE_ENABLE_UNREADS: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
